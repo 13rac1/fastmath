285b67b89f4b342a3082b57246e35b43ccbc8ad0
Brad Erickson <bderickson@gmail.com>
2019-08-09 00:33:20 -0700

    feat: Tested/Benchmarked 1:1 translation of FastLED's sin16()

--- trig.go
+++ trig.go
@@ -3,6 +3,9 @@ package fastmath
 // PI8 is the value of π in 8 bit math.
 const PI8 uint8 = 127
 
+// PI16 is the value of π in 16 bit math.
+const PI16 uint16 = 32768
+
 var bM16Interleave = []uint8{0, 49, 49, 41, 90, 27, 117, 10}
 
 // Sin8 is a fast 8-bit approximation of sin(x). This approximation never varies
@@ -46,4 +49,37 @@ func Sin8(theta uint8) uint8 {
 
 	y += 128
 	return uint8(y)
+}
+
+// Sin16 is a fast 16-bit approximation of sin(x). This approximation never varies more than
+// 0.69% from the floating point value.
+//
+// This is a Golang translation of the FastLED lib8tion sin16_C() function.
+// https://raw.githubusercontent.com/FastLED/FastLED/dcbf3993/lib8tion/trig8.h
+//
+// @param theta input angle from 0-65535
+// @returns sin of theta, value between -32767 to 32767.
+func Sin16(theta uint16) int16 {
+	var base = []uint16{0, 6393, 12539, 18204, 23170, 27245, 30273, 32137}
+	var slope = []uint8{49, 48, 44, 38, 31, 23, 14, 4}
+
+	offset := (theta & 0x3FFF) >> 3 // 0..2047
+	if theta&0x4000 != 0 {
+		offset = 2047 - offset
+	}
+
+	section := offset / 256 // 0..7
+	b := base[section]
+	m := slope[section]
+
+	secoffset8 := uint8(offset) / 2
+
+	mx := int16(m) * int16(secoffset8)
+	y := int16(mx) + int16(b)
+
+	if theta&0x8000 != 0 {
+		y = -y
+	}
+
+	return y
 }
