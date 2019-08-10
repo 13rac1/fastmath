3800a91aa8d50b470c9e9c5734add6d48c9dbdf2
Brad Erickson <bderickson@gmail.com>
2019-08-09 20:27:10 -0700

    chore: Add Cos8/Cos16 implementations using sine funcs

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # FastMath for Go
 
-Math functions when speed matters more than precision.
+8 and 16 bit math functions for when speed matters more than precision.
 
 > "I can make things very fast if they don’t have to be correct." — Russ Cox
 
 * Designed for use with Tiny Go.
 * Based on FastLED's lib8tion
 
-## Functions (TODO)
+## Functions
 
 * `Sin8()` / `Sin16()`
 * `Cos8()` / `Cos16()`
+
+## TODO
+
 * `Scale8()` / `Scale8Video()`
 * `Nscale8x3()` / `Nscale8x3Video()`
 * `Scale16()`
--- trig.go
+++ trig.go
@@ -1,5 +1,12 @@
 package fastmath
 
+// Trig Fast trig functions
+//
+// Fast 8 and 16-bit approximations of sin(x) and cos(x).
+// Don't use these approximations for calculating the
+// Trajectory of a rocket to Mars, but they're great
+// for art projects and LED displays.
+
 // PI8 is the value of π in 8 bit math.
 const PI8 uint8 = 127
 
@@ -14,8 +21,8 @@ var bM16Interleave = []uint8{0, 49, 49, 41, 90, 27, 117, 10}
 // This is a Golang translation of the FastLED lib8tion sin8_C() function.
 // https://raw.githubusercontent.com/FastLED/FastLED/dcbf3993/lib8tion/trig8.h
 //
-// @param theta input angle from 0-255.
-// @returns sin of theta, value between 0 and 255
+// Accepts theta input angle from 0-255.
+// Returns sin of theta, value between 0 and 255
 func Sin8(theta uint8) uint8 {
 	offset := theta
 	if theta&0x40 != 0 {
@@ -51,17 +58,26 @@ func Sin8(theta uint8) uint8 {
 	return uint8(y)
 }
 
+// Cos8 is a fast 8-bit approximation of cos(x). This approximation never varies
+// more than 2% from the floating point value.
+//
+// Accepts theta input angle from 0-255
+// Returns sin of theta, value between 0 and 255
+func Cos8(theta uint8) uint8 {
+	return Sin8(theta + 64)
+}
+
 var base = []uint16{0, 6393, 12539, 18204, 23170, 27245, 30273, 32137}
 var slope = []uint8{49, 48, 44, 38, 31, 23, 14, 4}
 
-// Sin16 is a fast 16-bit approximation of sin(x). This approximation never varies more than
-// 0.69% from the floating point value.
+// Sin16 is a fast 16-bit approximation of sin(x). This approximation never
+// varies more than 0.69% from the floating point value.
 //
 // This is a Golang translation of the FastLED lib8tion sin16_C() function.
 // https://raw.githubusercontent.com/FastLED/FastLED/dcbf3993/lib8tion/trig8.h
 //
-// @param theta input angle from 0-65535
-// @returns sin of theta, value between -32767 to 32767.
+// Accepts theta input angle from 0-65535.
+// Returns sin of theta, value between -32767 to 32767.
 func Sin16(theta uint16) int16 {
 	offset := (theta & 0x3FFF) >> 3 // 0..2047
 	if theta&0x4000 != 0 {
@@ -82,4 +98,13 @@ func Sin16(theta uint16) int16 {
 	}
 
 	return y
+}
+
+// Cos16 is a fast 16-bit approximation of cos(x). This approximation never
+// varies more than 0.69% from the floating point value.
+//
+// Accepts theta input angle from 0-65535.
+// Returns sin of theta, value between -32767 to 32767.
+func Cos16(theta uint16) int16 {
+	return Sin16(theta + 16384)
 }
